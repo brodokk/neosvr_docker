170e5e89aa79cfd7808a75013b87b418a3cd430c
brodokk <brodokk@brodokk.space>
2021-05-30 14:46:19 +0200

    Add cleaning

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,9 @@ case "$arguments" in
     echo "Run custom commands : ${arguments}"
     /bin/sh -c "$arguments";;
   *)
-    echo "Run NeosVR headless client"
+    echo "Cleaning old stuff..."
+    find /steam/${LAUNCH_APP}/Data/Assets -atime +7 -delete
+    find /steam/${LAUNCH_APP}/Data/Cache -atime +7 -delete
+    echo "Run NeosVR headless client..."
     cd /steam/${LAUNCH_APP}/ && mono Neos.exe;;
 esac
